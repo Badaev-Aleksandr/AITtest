bfc672820ee630298e6491209666bfa0e7b1764d
Aleksandr Badaev <badaev92aleksandr@gmail.com>
2024-03-19 18:56:18 +0100

    class Main was added

---
+++ src/main/java/aitlessons/Main.java
@@ -0,0 +1,9 @@
+package aitlessons;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello");
+        int a = 1;
+        java.lang.String hello = "aaa";
+    }
+}
